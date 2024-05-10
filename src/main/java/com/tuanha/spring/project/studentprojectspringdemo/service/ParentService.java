5e762255d40d17a2d4d2bf8e4fdee0044c0b0bb2
canngocduc <duc.can@bmbsoft.com.vn>
2024-05-10 17:46:59 +0700

    09/05/2024: change mapper -> FnMapper on ParentService

--- src/main/java/com/tuanha/spring/project/studentprojectspringdemo/service/ParentService.java
+++ src/main/java/com/tuanha/spring/project/studentprojectspringdemo/service/ParentService.java
@@ -2,51 +2,71 @@ package com.tuanha.spring.project.studentprojectspringdemo.service;
 
 import com.tuanha.spring.project.studentprojectspringdemo.dto.ParentDto;
 import com.tuanha.spring.project.studentprojectspringdemo.entity.Parent;
+import com.tuanha.spring.project.studentprojectspringdemo.exceptionstudent.NotFoundException;
+import com.tuanha.spring.project.studentprojectspringdemo.mapper.FnMapper;
 import com.tuanha.spring.project.studentprojectspringdemo.mapper.ParentMapper;
 import com.tuanha.spring.project.studentprojectspringdemo.repo.ParentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Supplier;
 
 @RequiredArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 @Service
 public class ParentService {
     ParentRepository parentRepository;
-    ParentMapper mapper;
+    private final ParentMapper parentMapper;
+    //ParentMapper mapper;
 
     public Parent saveParentStudent(ParentDto parentDTO) {
-        return parentRepository.save(mapper.dtoEntity().apply(parentDTO));
+        return parentRepository.save(FnMapper.createEntity(Parent::new,
+                parent -> BeanUtils.copyProperties(parentDTO, parent)));
     }
 
+
     public ParentDto getByIdParentStudent(Integer id) {
-        return mapper.entityToDTO().apply(parentRepository.findById(id).orElse(null));
+        Parent parent = parentRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Parent not found with id" + id));
+        return FnMapper.createEntity(ParentDto::new,
+                parentMapper.mapperEntityToDto(parent)
+                        .andThen(FnMapper.consumer(parent)));
     }
 
     public List<ParentDto> getAllParentStudent() {
-        return mapper.listEntityToDTO().apply(parentRepository.findAll());
+        return FnMapper.<Parent, ParentDto>mapperList(ParentDto::new).apply(parentRepository.findAll());
     }
     public List<ParentDto> getAllById(List<Integer> ids) {
-        return mapper.listEntityToDTO().apply(parentRepository.findAllById(ids));
+        List<Parent> parents = parentRepository.findAllById(ids);
+        if(parents.isEmpty()){
+            throw new NotFoundException("Parent not found with ID: " + ids);
+        }
+        return FnMapper.<Parent, ParentDto>mapperList(ParentDto::new).apply(parents);
     }
 
     //JPA Repository
     public Parent saveParent(ParentDto parentDTO){
-        return parentRepository.save(mapper.dtoEntity().apply(parentDTO));
+        return parentRepository.save(FnMapper.createEntity(Parent::new,
+                parent -> BeanUtils.copyProperties(parentDTO, parent)));
     }
 
     public List<ParentDto> getAllParent(){
-        return mapper.listEntityToDTO().apply(parentRepository.findAll());
+        return FnMapper.<Parent, ParentDto>mapperList(ParentDto::new).apply(parentRepository.findAll());
     }
 
     public ParentDto getByIdParent(Integer id){
-        return mapper.entityToDTO().apply(parentRepository.findById(id).get());
+        Parent parent = parentRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Parent not found with id" + id));
+        return FnMapper.createEntity(ParentDto::new,
+                parentMapper.mapperEntityToDto(parent)
+                        .andThen(FnMapper.consumer(parent)));
     }
     public Optional<ParentDto> getAllById(Integer id){
-        return Optional.ofNullable(mapper.entityToDTO().apply(parentRepository.findAllById(id).get()));
+        return Optional.ofNullable(FnMapper.dtoEntity(ParentDto::new).apply(parentRepository.findAllById(id).get()));
     }
 
 }
